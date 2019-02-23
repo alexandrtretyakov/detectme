ece5efc648a6a9496b63f3fda16f19938d94e07e
Alexandr Tretyakov <alex@tretyakov.net>
2019-02-23 09:47:21 +0300

    Захват видео с веб-камеры

---
+++ capture.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+
+# Захватываем видео поток с камеры
+cap = cv2.VideoCapture(0)
+
+print('Для выхода нажмите ESC')
+
+while True:
+    ret, frame = cap.read()
+
+    # Добавляем еще один в черно-белом формате
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Main frame', frame)
+    cv2.imshow('Gray frame', gray)
+    if cv2.waitKey(10) == 0x1b:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
